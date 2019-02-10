aeece2d3681d087de3c471a78e37bad01166e9f5
donghwipark <35325234+donghwipark@users.noreply.github.com>
2019-02-10 20:33:29 +0900

    signupAboutme
    done basic setting delete the basics

--- app/screens/signupAboutMeInfo.js
+++ app/screens/signupAboutMeInfo.js
@@ -10,82 +10,20 @@ import {
 } from 'react-native';
 import {MaterialCommunityIcons, FontAwesome, MaterialIcons} from '@expo/vector-icons'
 
+//will add bubble functions to select 8 keywords
+
 export default class SignUp extends React.Component {
   state = {
-    email: '',
-    password: '',
-    nickname: '',
+    aboutme: ''
   }
-  handleEmail = (text) => {
-    this.setState({ email: text })
+
+  handleAboutMeInfo = (text) => {
+    this.setState({ aboutme: text })
   }
-  handlePassword = (text) => {
-    this.setState({ password: text })
-  }
-  handleNickname = (text) => {
-    this.setState({ nickname: text })
-  }
-  login = (email, pass) => {
-    alert('email: ' + email + ' password: ' + pass)
-  }
-  // signIn = () => {
-  //   return fetch("http://ec2-18-217-132-110.us-east-2.compute.amazonaws.com:3005/api/users")
-  //     .then((response) => response.json())
-  //     .then((response) => {
-  //       const checkEmailAndPassword = (element) => {
-  //         if ( (element.email ===this.state.email) && (element.password === this.state.password) ) {
-  //           return true
-  //         }
-  //       }        
-  //       if (response.some(checkEmailAndPassword)) {
-  //         this.props.onPress()  
-  //       } else {
-  //         alert('Email or password is wrong')
-  //       }     
-  //     })
-  //     .catch((error) => {
-  //       console.error(error)  
-  //   })
-  // }
+
   render () {
     return (
       <View style={{flex:1, flexDirection:"column", margin:10}}>
-        <View style={{marginTop:150}}>
-          <View style={styles.buttoncontainer}>
-            <MaterialCommunityIcons name="email" size={20} color={'black'}/>
-            <TextInput style = {styles.input}
-              placeholder='    Email'
-              onChangeText={(text) => this.setState({email: text})}
-            />
-          </View>
-        </View>
-        <View>
-          <View style={styles.buttoncontainer}>
-            <MaterialCommunityIcons name="account-location" size={20} color={'black'}/>  
-            <TextInput style = {styles.input}
-              placeholder='    Nickname'
-              onChangeText={(text) => this.setState({password: text})}
-            />
-          </View>  
-        </View>
-        <View>
-          <View style={styles.buttoncontainer}>
-            <FontAwesome name="birthday-cake" size={20} color={'black'}/>  
-            <TextInput style = {styles.input}
-              placeholder='    Birthday'
-              onChangeText={(text) => this.setState({password: text})}
-            />
-          </View>  
-        </View>
-        <View>
-          <View style={styles.buttoncontainer}>
-            <MaterialIcons name="location-city" size={20} color={'black'}/>  
-            <TextInput style = {styles.input}
-              placeholder='    Location city'
-              onChangeText={(text) => this.setState({password: text})}
-            />
-          </View>  
-        </View>
         <View>
           <View style={styles.buttoncontainer}>
             <FontAwesome name="heart" size={20} color={'black'}/>  
@@ -95,35 +33,6 @@ export default class SignUp extends React.Component {
             />
           </View>  
         </View>
-        <View>
-          <View style={styles.buttoncontainer}>
-            <MaterialCommunityIcons name="human-male-female" size={20} color={'black'}/>  
-            <TextInput style = {styles.input}
-              placeholder='    Gender'
-              onChangeText={(text) => this.setState({password: text})}
-            />
-          </View>  
-        </View> 
-        <View>
-          <View style={styles.buttoncontainer}>
-            <FontAwesome name="user-secret" size={20} color={'black'}/>  
-            <TextInput style = {styles.input}
-              placeholder='    Password'
-              secureTextEntry
-              onChangeText={(text) => this.setState({password: text})}
-            />
-          </View>  
-        </View>
-        <View>
-          <View style={styles.buttoncontainer}>
-            <FontAwesome name="user-secret" size={20} color={'black'}/>  
-            <TextInput style = {styles.input}
-              placeholder='    Checking password'
-              secureTextEntry
-              onChangeText={(text) => this.setState({password: text})}
-            />
-          </View>  
-        </View> 
         <TouchableHighlight style={styles.buttoncontainer} onPress={this.signIn}>        
           <View style={styles.loginButtoncontainer}>
             <FontAwesome name="sign-in" size={20} color={'#495057'}/>
